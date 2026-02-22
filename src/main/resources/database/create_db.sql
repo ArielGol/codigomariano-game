5bcb5fd27fd33b6b143a1751c59140bfd2766baf
Ariel Golubickas <ariel00golubickas@gmail.com>
2026-02-22 20:49:51 -0300

    correcciones de los archivos sql

--- src/main/resources/database/create_db.sql
+++ src/main/resources/database/create_db.sql
@@ -1,26 +1,48 @@
+create table CATEGORIAS(
+	ID integer NOT NULL,
+	Nombre varchar(255) NOT NULL,
+	Key varchar(255) NOT NULL,
+	PRIMARY KEY(ID)
+);
+create table OPCIONES(
+	ID integer NOT NULL,
+	Caracter char NOT NULL,
+	PRIMARY KEY(ID)
+);
+create table OPCIONES_BINARIAS(
+	ID integer NOT NULL,
+	Letra char NOT NULL,
+	Texto varchar(255) NOT NULL,
+	Opcion boolean NOT NULL,
+	PRIMARY KEY(ID)
+);
+
 create table USUARIOS(
 	ID integer NOT NULL AUTO_INCREMENT,
 	Email varchar(75) NOT NULL,
+	Juego_id integer NOT NULL,
 	PRIMARY KEY(ID)
 );
 
 create table JUEGOS(
-	ID integer not null,
+	ID integer NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY(ID)
 );
 
-alter table USUARIOS add constraint USU$JUE foreign key(ID) references JUEGOS(ID);
+alter table USUARIOS add constraint USU$JUE foreign key(Juego_id) references JUEGOS(ID);
 
 create table PREGUNTAS(
 	ID integer NOT NULL AUTO_INCREMENT,
-	Codigo varchar(255),
-	Texto text(65535),
+	Codigo varchar(255) NOT NULL,
+	Texto text(65535)NOT NULL,
 	Puntaje integer,
-	Opciones varchar(255),
-	Correcta varchar(255),
+	Opciones varchar(255) NOT NULL,
+	Correcta varchar(255) NOT NULL,
 	Categoria_id integer NOT NULL,
 	PRIMARY KEY(ID)
 );
+
+alter table PREGUNTAS add constraint PRE$CAT foreign key(Categoria_id) references CATEGORIAS(ID);
 
 create table PREGUNTAS_JUEGOS(
 	Pregunta_id integer NOT NULL,
@@ -29,15 +51,8 @@ create table PREGUNTAS_JUEGOS(
 );
 
 alter table PREGUNTAS_JUEGOS add constraint PJ$PRE foreign key(Pregunta_id) references PREGUNTAS(ID);
-
 alter table PREGUNTAS_JUEGOS add constraint PJ$JUE foreign key(Juego_id) references JUEGOS(ID);
 
-create table CATEGORIAS(
-	ID integer NOT NULL,
-	Nombre varchar(255) NOT NULL,
-	Key varchar(255) NOT NULL,
-	PRIMARY KEY(ID)
-);
 create table MULTIPLES(
 	ID integer NOT NULL,
 	Opcion_id integer NOT NULL,
@@ -45,25 +60,14 @@ create table MULTIPLES(
 );
 
 alter table MULTIPLES add constraint MUL$PRE foreign key(ID) references PREGUNTAS(ID);
-
-create table OPCIONES(
-	ID integer NOT NULL,
-	Caracter char NOT NULL,
-	PRIMARY KEY(ID)
-);
+alter table MULTIPLES add constraint MUL$OPC foreign key(Opcion_id) references OPCIONES(ID);
 
 create table BINARIAS(
 	ID integer NOT NULL,
 	Opcion_binaria_id integer NOT NULL,
 	PRIMARY KEY(ID)
 );
-
 alter table BINARIAS add constraint BIN$PRE foreign key(ID) references PREGUNTAS(ID);
+alter table BINARIAS add constraint BIN$OPB foreign key(Opcion_binaria_id) references OPCIONES_BINARIAS(ID);
 
-create table OPCIONES_BINARIAS(
-	ID integer NOT NULL,
-	Letra char NOT NULL,
-	Texto varchar(255) NOT NULL,
-	Opcion boolean NOT NULL,
-	PRIMARY KEY(ID)
-);
+
--- src/main/resources/database/init_db.sql
+++ src/main/resources/database/init_db.sql
@@ -5,12 +5,6 @@ insert into CATEGORIAS(ID,Nombre) values(3,'CIENCIA');
 insert into CATEGORIAS(ID,Nombre) values(4,'DEPORTES');
 insert into CATEGORIAS(ID,Nombre) values(5,'HISTORIA');
 
-insert into CATEGORIAS(ID,Key) values(0,'geography');
-insert into CATEGORIAS(ID,Key) values(1,'art and literature');
-insert into CATEGORIAS(ID,Key) values(2,'entertainment');
-insert into CATEGORIAS(ID,Key) values(3,'nature');
-insert into CATEGORIAS(ID,Key) values(4,'sports');
-insert into CATEGORIAS(ID,Key) values(5,'history');
 
 insert into OPCIONES(ID,Caracter) values(0,'A');
 insert into OPCIONES(ID,Caracter) values(1,'B');
@@ -19,7 +13,3 @@ insert into OPCIONES(ID,Caracter) values(3,'D');
 
 insert into OPCIONES_BINARIAS(ID,Letra) values(0,'A');
 insert into OPCIONES_BINARIAS(ID,Letra) values(1,'B');
-insert into OPCIONES_BINARIAS(ID,Texto) values(0,'Verdadero');
-insert into OPCIONES_BINARIAS(ID,Texto) values(1,'Falso');
-insert into OPCIONES_BINARIAS(ID,Opcion) values(0,'true');
-insert into OPCIONES_BINARIAS(ID,Opcion) values(1,'false');
